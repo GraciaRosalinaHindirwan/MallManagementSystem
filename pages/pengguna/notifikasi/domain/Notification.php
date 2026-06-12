32909c5dd3d0f12f072068683a0489f3d722bc96
rsnorlatch <muhfarisnew@gmail.com>
2026-06-12 18:14:01 +0700

    removed scratchpad temporarily

---
+++ pages/pengguna/notifikasi/domain/Notification.php
@@ -0,0 +1,15 @@
+<?php
+
+class Notification
+{
+    public int $id;
+    public string $message;
+    public DateTime $date_time;
+
+    public function __construct(int $id, string $message, DateTime $date_time)
+    {
+        $this->id = $id;
+        $this->message = $message;
+        $this->date_time = $date_time;
+    }
+}
--- pages/pengguna/notifikasi/persistence/queries/InMemoryNotifictionQuery.php
+++ pages/pengguna/notifikasi/persistence/queries/InMemoryNotifictionQuery.php
@@ -1,13 +1,28 @@
 <?php
 
+require_once __DIR__ . "/../../domain/User.php";
+require_once __DIR__ . "/../../INotificationQuery.php";
+
 class InMemoryNotifictionQuery implements INotificationQuery
 {
-    private array $notification;
+    private array $notifications;
 
     public function __construct(array $notification)
     {
-        $this->notification = $notification;
+        $this->notifications = $notification;
     }
 
-    public function get_by_id(int $id) {}
+    public function get_by_id(int $id)
+    {
+        $filtered = array_values(
+            array_filter(
+                $this->notifications,
+                function (Notification $n) use ($id) {
+                    return $n->id == $id;
+                }
+            )
+        );
+
+        return array_shift($filtered);
+    }
 }
