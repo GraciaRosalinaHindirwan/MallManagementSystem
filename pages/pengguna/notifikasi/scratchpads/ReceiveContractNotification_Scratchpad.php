e5c12c7637c300736696becd324e0b8acf0df82f
rsnorlatch <muhfarisnew@gmail.com>
2026-06-16 16:58:21 +0700

    mengubah usecase menerima notifikasi kontrak agar semua user yang pernah ada terpanggil daripada hanya terbatas oleh satu user saja

--- pages/pengguna/notifikasi/scratchpads/ReceiveContractNotification_Scratchpad.php
+++ pages/pengguna/notifikasi/scratchpads/ReceiveContractNotification_Scratchpad.php
@@ -9,17 +9,20 @@ require_once __DIR__ . "/../infrastructure/writer/InMemoryNotificationLogWriter.
 require_once __DIR__ . "/../domain/User.php";
 
 $user = new InMemoryUserQuery([
-    new User(1, "user1", "user1@gmail.com")
+    new User(1, "user1", "user1@gmail.com"),
+    new User(2, "user2", "user2@gmail.com"),
+    new User(3, "user3", "user3@gmail.com")
 ]);
 $notifier = new ConsoleNotifier();
 $contract = new InMemoryContractQuery([
-    new Contract(1, 1, new DateTime())
+    new Contract(1, 1, new DateTime()),
+    new Contract(2, 2, new DateTime()),
+    new Contract(3, 3, new DateTime())
 ]);
 
 $log_writer = new InMemoryNotificationLogWriter([]);
 
 $usecase = new ReceiveContractNotificationUseCase($notifier, $user, $contract, $log_writer);
-
-$usecase->execute(1);
+$usecase->execute();
 
 var_dump($log_writer->get_logs());
