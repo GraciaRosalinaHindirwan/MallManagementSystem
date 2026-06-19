a4fb4839cc9aab554f04ea2a86ee627bda34325c
rsnorlatch <muhfarisnew@gmail.com>
2026-06-19 09:01:48 +0700

    restructure user

--- pages/pengguna/notifikasi/domain/User.php
+++ pages/pengguna/notifikasi/domain/User.php
@@ -5,15 +5,34 @@ require_once __DIR__ . "/UserType.php";
 class User
 {
     public int $id;
+    public string $fullname;
     public string $username;
     public string $email;
-    public UserType $type;
+    public string $password;
+    public bool $must_change_password;
+    public int $role_page_id;
+    public int $failed_login_attempt;
+    public bool $is_blocked;
 
-    public function __construct(int $id, string $username, string $email, UserType $type)
-    {
+    public function __construct(
+        int $id,
+        string $fullname,
+        string $username,
+        string $email,
+        string $password,
+        bool $must_change_password,
+        int $role_page_id,
+        int $failed_login_attempt,
+        bool $is_blocked,
+    ) {
         $this->id = $id;
+        $this->fullname = $fullname;
         $this->username = $username;
         $this->email = $email;
-        $this->type = $type;
+        $this->password = $password;
+        $this->must_change_password = $must_change_password;
+        $this->role_page_id = $role_page_id;
+        $this->failed_login_attempt = $failed_login_attempt;
+        $this->is_blocked = $is_blocked;
     }
 }
