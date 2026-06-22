ee3df6381e4fb7dab37cecf9145ae35e1cc7bac5
fniazell <faniaazelia11@gmail.com>
2026-06-22 13:25:32 +0700

    update: ganti database ke mall_erp

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "mall_erp_global");
+$conn = new mysqli("localhost", "root", "", "mall_erp");
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
