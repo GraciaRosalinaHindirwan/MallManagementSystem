adfcbbce12b333cb4c31bb4320278f7145113189
fniazell <faniaazelia11@gmail.com>
2026-06-22 13:17:54 +0700

    update: sesuaikan database global

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "mall_manajemen");
+$conn = new mysqli("localhost", "root", "", "mall_erp_global");
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
