fa68d81bdd309c83e999e4859722e7538cce9975
sagi99 <donarobiha95@gmail.com>
2026-06-24 21:48:17 +0700

    fix logout path log_helper

--- public/auth/logout.php
+++ public/auth/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once '../config/log_helper.php';
+require_once '../../config/log_helper.php';
 
 $username = $_SESSION['username'] ?? 'Unknown';
 simpanLog($username, 'Logout');
