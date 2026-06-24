413c995ae4e74790a6e90e1bd582a1a1291860ae
Intan140 <124240140@student.upnyk.ac.id>
2026-06-24 20:33:02 +0700

    Add login form to index.php

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<!-- Contoh potongan tag form di index.php -->
+<form action="auth/loginProcess.php" method="POST">
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+</form>
